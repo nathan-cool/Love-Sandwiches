764185eb1375d98dc2cae61f5f8ad6fe1ea51704
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-01-24 20:48:42 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
+#!/usr/bin/python3
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREADS = Credentials.from_service_account_file('creds.json')
+SCOPE_CREDS = CREADS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
