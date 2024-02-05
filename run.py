7af87321b77b8bcadcf3cb21d81ee8400b8498ca
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-02-05 08:40:43 +0000

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -18,5 +20,15 @@ sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
 
 print(data)
+print("hello world")
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+def get_sales_data():
+    """Get sales figures input from the user"""
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+    input_data = input("Enter your data here: ")
+    print(f"The data provided is {input_data}")
+ 
+get_sales_data()
