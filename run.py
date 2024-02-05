286423fc75ca86c8c8ad62e6b06df2f8a7e85804
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-02-05 09:28:49 +0000

    Updated logic

--- run.py
+++ run.py
@@ -25,10 +25,32 @@ print("hello world")
 
 def get_sales_data():
     """Get sales figures input from the user"""
+    
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
-    input_data = input("Enter your data here: ")
-    print(f"The data provided is {input_data}")
+    
+    data_str = input("Enter your data here: ")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
  
+def validate_data(values):
+    """
+    Inside this try coverts all string values to ints. 
+    raises ValueError if strings cannot be converted. 
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values are required, you provided {len(values)}"
+            )
+    
+    except ValueError as e: 
+        print(f"Invalid data: {e} \n")
+ 
+ 
+ 
+ 
+
 get_sales_data()
